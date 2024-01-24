3fb3b25548688b42b402433777056215a9c44eba
IsaganiJulian <100246651+IsaganiJulian@users.noreply.github.com>
2024-01-24 14:42:08 -0600

    Update Sql Query For Flight Ops
    SQL EDA

---
+++ flight_ops/flight_ops_querry/flight_ops_AVG_Arrival_Ops_Count.sql
@@ -0,0 +1,6 @@
+SELECT "Report_Period", "Arrival_Departure", AVG("Flight_Ops_Count") as Average_Flight_Ops_Count 
+FROM "flight_ops_data"
+WHERE "Arrival_Departure" = ('Arrival')
+GROUP BY "Report_Period", "Arrival_Departure"
+ORDER BY Average_Flight_Ops_Count DESC
+LIMIT 10;
